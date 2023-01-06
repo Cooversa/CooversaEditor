a799091f1312b8abdb955d5769ed2787ec1479b7
sheyzi <gistkiosk@gmail.com>
2023-01-06 02:53:31 +0100

    Init Repo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import TipTap from "../libs/TipTap.svelte";
+</script>
+
+<svelte:head>
+    <title>Cooversa Editor</title>
+</svelte:head>
+
+
+<div class="m-5 relative">
+    <div class="my-5">
+        <h1 class="mb-2 text-3xl font-semibold">Cooversa Editor</h1>
+        <p class="text-sm">
+            This tool makes it easier for cooversa teachers to create course contents and at the same time see
+            a preview of what it could look like
+        </p>
+    </div>
+    <TipTap />
+</div>
